50436f3a67ca4cfc7d27d92d09bc041e81783386
Matthew Dong <mattdong1231@gmail.com>
2024-10-05 21:45:54 -0400

    module 3

--- CarsApp/CarsApp/TODO/CarCatalog.swift
+++ CarsApp/CarsApp/TODO/CarCatalog.swift
@@ -16,26 +16,35 @@ import Foundation
 
 class CarCatalog {
 
-    var carsAvailable: [CarModel : Int] // A mapping of the car models to the amount of these models available.
-    var displayedCars: [CarModel]
-    var favourites: [CarModel]
+    var carsAvailable: [CarModel: Int] // A mapping of the car models to the amount of these models available.
+    var displayedCars: [CarModel] // Cars that are currently displayed to the user
+    var favourites: [CarModel] // A list of favorite cars
     
     /*
      TODO: write an initializer that fetches all the car models. For the purposes of this assignment, initialize carsAvailable to a default array of 5-6 car models of your choice, with an arbitrary number representing their amount. Make sure the displayedCars are initialized with all the cars in carsAvailable. Upon launch, the user should have no favourites (empty).
      NOTE: The ids of the cars you provide must be unique, otherwise the app will crash.
     */
     init() {
-
+        let car1 = CarModel(id: "1", brand: CarBrand(rawValue: "Mercedes") ?? .none, name: "G-Wagon", topSpeed: 120, color: "Red", fuelCapacity: 100)
+        let car2 = CarModel(id: "2", brand: CarBrand(rawValue: "BMW") ?? .none, name: "X5", topSpeed: 140, color: "Black", fuelCapacity: 90)
+        let car3 = CarModel(id: "3", brand: CarBrand(rawValue: "Toyota") ?? .none, name: "Corolla", topSpeed: 110, color: "White", fuelCapacity: 50)
+        let car4 = CarModel(id: "4", brand: CarBrand(rawValue: "Ford") ?? .none, name: "Mustang", topSpeed: 150, color: "Blue", fuelCapacity: 80)
+        
+        carsAvailable = [car1: 10, car2: 8, car3: 12, car4: 5]
+        displayedCars = Array(carsAvailable.keys)
+        favourites = [] 
     }
-    
     
     /*
         TODO: implement this function
         This function gets called by the UI whenever the user wants to ADD/REMOVE this car from their favourites list. Change the state of the favourites array to accomplish this.
      */
     func toggleFavourite(car: CarModel) {
-        
- 
+        if let index = favourites.firstIndex(of: car) {
+            favourites.remove(at: index) // Remove if it's already a favourite
+        } else {
+            favourites.append(car) // Add to favourites if not already in
+        }
     }
     
     /*
@@ -43,8 +52,7 @@ class CarCatalog {
         This function gets called by the UI whenever the user wants to view the amount of cars available from a certain model.
      */
     func getCarAvailability(car: CarModel) -> Int {
-        
-
+        return carsAvailable[car] ?? 0 // Return the availability, default to 0 if not found
     }
     
     /*
@@ -53,9 +61,10 @@ class CarCatalog {
         NOTE: make sure you handle the case where it's 'none', as it implies that we don't want a filter.
      */
     func filterCarsBy(brand: CarBrand) {
-
+        if brand == .none {
+            displayedCars = Array(carsAvailable.keys) // No filter, show all cars
+        } else {
+            displayedCars = carsAvailable.keys.filter { $0.brand == brand }
+        }
     }
-    
-    
-    
 }
