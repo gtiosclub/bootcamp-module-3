f013e0661284381a9065a7d28f8f9c7464b18d44
gellenee <glennedelatorre@gmail.com>
2024-10-06 21:25:37 -0400

    module 3 car model

--- CarsApp/CarsApp/TODO/CarModel.swift
+++ CarsApp/CarsApp/TODO/CarModel.swift
@@ -34,20 +34,25 @@ struct CarModel: Identifiable, Hashable {
     let id: String // DO NOT EDIT THIS. This is the model id, and is used by the UI.
     
     // TODO: complete the struct's implementation
+    let brand: CarBrand
+    let name: String
+    let topSpeed: Int
+    let color: String
+    let fuelCapacity: Int
     
     
     // TODO: These are functions used by the UI to display the model's data, right now they return default values. Fix it such that it returns the correct value.
     
     func getBrandYear() -> String {
-        return "1920"
+        return brand.carYear
     }
     
     func getBrandFact() -> String {
-        return "Mercedes Benz invented the first gasoline-powered automobile."
+        return brand.carFact
     }
     
     func getBrandName() -> String {
-        return "Mercedes"
+        return brand.carName
     }
     
     func getModelID() -> String {
@@ -55,19 +60,19 @@ struct CarModel: Identifiable, Hashable {
     }
     
     func getModelName() -> String {
-        return "G-Wagon"
+        return name
     }
     
     func getTopSpeed() -> Int {
-        return 120
+        return topSpeed
     }
     
     func getColor() -> String {
-        return "Red"
+        return color
     }
     
     func getFuel() -> Int {
-        return 100
+        return fuelCapacity
     }
     
 }
